e95f836f34ff4e6ac6a7b0f0a530a5f8c326dcec
Mike <miikesantos25@gmail.com>
2022-06-23 00:58:52 -0300

    Variaveis

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,5 +1,8 @@
-let nome = "Mike"
-let email = "mike@gmail.com"
-let profissao = "Editor"
-let idade = 29
-let lugar dos sonhos = "China"
+console.log("Você por aqui de novo?")
+let nome = "Pedro"
+console.log(nome)
+console.log("Já sei seu email")
+let email = "pedro.p@gmail.com"
+console.log("pedro.p@gmail.comail")
+
+
