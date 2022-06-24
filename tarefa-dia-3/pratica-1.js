dc17a8223a44eccd881c387eeaf33400c931ad7b
Mike <miikesantos25@gmail.com>
2022-06-23 23:58:12 -0300

    Conseguiu entender o comando IF e ELSE

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -1,8 +1,14 @@
-console.log("Você por aqui de novo?")
-let nome = "Pedro"
-console.log(nome)
-console.log("Já sei seu email")
-let email = "pedro.p@gmail.com"
-console.log("pedro.p@gmail.comail")
+//console.log("Você por aqui de novo?")
+//let nome = "Pedro"
+//console.log(nome)
+//console.log("Já sei seu email")
+//let email = "pedro.p@gmail.com"
+//console.log("pedro.p@gmail.comail")
 
+
+
+let nome ="Pedro"
+let idade =39
+let profissao ="Cachaceiro"
+console.log ("Olá, "nome"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + email + " Confirma pra gente alguns dados? A sua idade é <valor da " + idade + ", você é " + profissao + "  e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
 
